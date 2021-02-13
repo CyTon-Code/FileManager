93dbdab39e54d247d7b10ac2e9f46ab5eef7dad6
CyTon Program Languages <cytoncode@gmail.com>
2021-02-13 21:41:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-no_exit = True
-result = None
-while no_exit:
-    if result[0] == "exit":
-        break
+# import  as to_list
+hi_sms = "$ "
+# from ant import path
+
+
+def mode_manual():  # Manual command input mode:
+    no_exit = True
+    command = None
+    
+    while no_exit:
+        command = to_list(input(hi_sms))
+        if command[0] == "exit":
+            no_exit = False
+    pass
+
+
+def mode_include(link: str):  # Executing commands from a file:
+    pass
+
+
+def mode_arguments():
+    pass
+
+
+mode_arguments()
+
+for link in path:
+    mode_include(link)
+
+mode_manual()
