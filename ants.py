ce0dd048ae530b135be4501629c40b9c90bc69f2
CyTon Code <cytoncode@gmail.com>
2021-02-16 20:49:24 +0200

    Update ants.py

--- ants.py
+++ ants.py
@@ -1,20 +1,36 @@
-def usr(arg):
-    l = [".q", ".quit", ".e", ".exit", ".c", ".close"]
-    if arg in l:
+def system(key, value=""):
+    if key in (".q", ".quit", ".e", ".exit", ".c", ".close"):  # Выйти:
         exit()
+
+    elif key in (".i", ".info"):  # Узнать подробнее об args команды:
+        pass
+
+    elif key in (".h", ".help"):  # Получить список args команды:
+        pass
+
+    elif key in (".d", ".doc"):  # Полная документация об команде:
+        pass
+
+    elif key in (".v", ".version"):  # Узнать версию команды.
+        pass
+
+    else:
+        print("key: {key} not difened!".format(key))
 
 
 def main(args):
     no_exit = True
     command = None
-    
+
     while no_exit:
         command = to_list(input(hi_sms))
         type_ant = ant_typ(command)
-        
+
         if type_ant == "usr":  # .exit .help ...
             system(command[0])
+
         elif type_ant == "sys":
             pass  # import: home/sys   return bool or Exeption
+
         else:  # module:
             pass  # import: home/def
