2ef1fd6c148cb190be889927010134da3c13bad6
CyTon Code <cytoncode@gmail.com>
2021-02-28 15:38:05 +0200

    Update main

--- internal_modules/moves_module/main.py
+++ internal_modules/moves_module/main.py
@@ -1,16 +1,5 @@
 from   as check_file
 
-def not_really_or_really(link, path, result=True):
-    if check_file(link) or not check_file(path):  # if a or !b is true:
-        return result
-    return not result  # иначе: reverse
-
-
-def not_really_and_really(link, path, result=True):
-    if check_file(link) and not check_file(path):  # if a or !b is true:
-        return result
-    return not result  # иначе: reverse
-
 
 def moves(link, path):
     # если ссылки равны:
@@ -19,33 +8,14 @@ def moves(link, path):
     если "первая ссылка указывает не на файл" или (
             "вторая ссылка указывает на файл"):
         скажу "переместить файл не могу!"
-    
+    import os
     переместить файл(с link к path)
+    # os.replace(link, path)  # Перемещаю файл.
     
-    если "первая ссылка указывает не на файл" или (
+    # после перемещения: a должен исчезнуть, а b должен появится.
+    если "первая ссылка указывает не на файл" и (
             "вторая ссылка указывает на файл"):
         скажу "переместить файл удалось!"
     иначе:
         скажу "эта операция не сработала корректно!"
-    # если "link указывает не на файл" или "path указывает на файл":
-    #     скажу "Я не могу переместить."
-    #скажу = (ссылка на файл которого недолжно быть, ссылка на файл
-    #который есть, значение которое нужно вернуть если ссылки коректны)
-    
-    if not_really_or_really(link, path):  # Если переместить нереально:
-        return None
-    # скажу "Я могу переместить файл.":
-    
-    os.replace(link, path)  # Перемещаю файл.
-    
-    # после перемещения: a должен исчезнуть, а b должен появится.
-    
-    if not_really_and_really(link, path):
-        pass
-    
-    если "link указывает не на файл" или "path указывает на файл":
-        скажу "Я смогла переместить файл."
-    иначе:
-        скажу "Я не смогла переместить файл."
-    return скажу
 
